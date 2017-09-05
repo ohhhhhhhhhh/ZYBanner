0ee145e0f1aeb9a27973a2ac47053112b6ff5beb
zy <zhangyue@renwohua.com>
2017-09-05 11:27:39 +0800

    Initial Commit

---
+++ ZYBanner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZYBanner
+//
+//  Created by zy on 2017/9/5.
+//  Copyright © 2017年 zy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
